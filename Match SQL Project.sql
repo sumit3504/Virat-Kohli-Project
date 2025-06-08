aa5f07e94a7e7bf837dea4325bf478a9a6ae13ca
sumit3504 <sumit.kumar352004@gmail.com>
2025-06-08 10:54:50 +0530

    Update Match SQL Project.sql

--- Match SQL Project.sql
+++ Match SQL Project.sql
@@ -1,46 +1,69 @@
-create database viratkohlimatch;
-use viratkohlimatch;
-CREATE TABLE KohliStats (
-    match_id INT PRIMARY KEY,
-    opponent VARCHAR(50),
-    format VARCHAR(10), -- 'ODI', 'T20', 'Test'
+CREATE DATABASE viratkohlimatch;
+USE viratkohlimatch;
+
+CREATE TABLE KohliYearlyStats (
+    format VARCHAR(20),
+    year INT,
+    innings INT,
     runs INT,
-    balls_faced INT,
+    balls INT,
+    outs INT,
+    avg DECIMAL(5,1),
+    strike_rate DECIMAL(5,1),
+    high_score INT,
+    fifties INT,
+    centuries INT,
     fours INT,
     sixes INT,
-    not_out BOOLEAN
+    dot_percentage DECIMAL(5,1)
 );
-INSERT INTO KohliStats VALUES
-(1, 'Australia', 'ODI', 115, 105, 12, 2, FALSE),
-(2, 'England', 'Test', 35, 50, 4, 0, FALSE),
-(3, 'South Africa', 'T20', 72, 45, 6, 3, TRUE),
-(4, 'Pakistan', 'ODI', 5, 10, 0, 0, FALSE),
-(5, 'New Zealand', 'ODI', 138, 121, 14, 4, FALSE),
-(6, 'Sri Lanka', 'ODI', 89, 78, 8, 1, FALSE),
-(7, 'Bangladesh', 'Test', 254, 289, 22, 7, TRUE),
-(8, 'West Indies', 'ODI', 45, 39, 5, 0, FALSE),
-(9, 'Afghanistan', 'T20', 63, 52, 7, 1, TRUE);
 
-SELECT MAX(runs) AS max_runs FROM KohliStats;
+INSERT INTO KohliYearlyStats VALUES
+('Test', 2017, 16, 1059, 1389, 14, 75.6, 76.2, 243, 1, 5, 98, 6, 54.2),
+('Test', 2016, 18, 1215, 2011, 16, 75.9, 60.4, 235, 2, 4, 134, 2, 65.1),
+('Test', 2024, 19, 417, 673, 17, 24.5, 62.0, 100, 1, 1, 43, 5, 67.0),
+('ODI', 2018, 14, 1202, 1172, 9, 133.6, 102.6, 160, 3, 6, 123, 13, 41.7),
+('T20I', 2016, 13, 641, 457, 6, 106.8, 140.3, 90, 7, 0, 70, 9, 29.1),
+('TWENTY20', 2016, 16, 973, 640, 12, 81.1, 152.0, 113, 7, 4, 84, 38, 26.4);
 
-SELECT MIN(runs) AS min_runs FROM KohliStats;
+SELECT * FROM KohliYearlyStats;
 
-SELECT MAX(sixes) AS max_sixes FROM KohliStats;
+SELECT * FROM KohliYearlyStats
+WHERE Format = 'Test';
 
-SELECT MAX(fours) AS max_fours FROM KohliStats;
+SELECT Format, SUM(Runs) AS TotalRuns
+FROM KohliYearlyStats
+GROUP BY Format;
 
-SELECT COUNT(*) AS full_centuries
-FROM KohliStats
-WHERE runs >= 100;
+SELECT year,runs
+FROM KohliYearlyStats
+ORDER BY runs DESC
+LIMIT 5;
 
-SELECT COUNT(*) AS half_centuries
-FROM KohliStats
-WHERE runs >= 50 AND runs < 100;
+SELECT Format, Year, strike_rate
+FROM KohliYearlyStats
+ORDER BY strike_rate DESC
+LIMIT 3;
 
-SELECT COUNT(*) AS total_matches FROM KohliStats;
+SELECT year, format, runs
+FROM KohliYearlyStats
+WHERE runs > 1000;
 
-SELECT SUM(runs) AS total_runs FROM KohliStats;
+SELECT Format, Year, high_score
+FROM KohliYearlyStats
+ORDER BY high_score DESC;
 
+SELECT * FROM KohliYearlyStats
+WHERE Year = 2016;
 
+SELECT AVG(Sixes) AS AverageSixes FROM KohliYearlyStats;
 
+SELECT Format, Year, Sixes
+FROM KohliYearlyStats
+ORDER BY Sixes DESC
+LIMIT 5;
 
+SELECT Format, Year, Fours
+FROM KohliYearlyStats
+ORDER BY Fours DESC
+LIMIT 3;
